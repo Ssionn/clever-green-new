d7486b54361d1435a758f991ad5dd4f1b8dde4aa
Ssionn <Ssionn@users.noreply.github.com>
2024-07-05 21:57:09 +0000

    Apply Laravel Pint changes

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,7 @@
 use App\Http\Controllers\LandingPageController;
 use Illuminate\Support\Facades\Route;
 
-
-Route::middleware(['web'])->group(function() {
+Route::middleware(['web'])->group(function () {
     Route::get('/', [LandingPageController::class, 'index'])->name('landingpage.index');
     Route::get('/about-us', [LandingPageController::class, 'about'])->name('landingpage.about');
     Route::get('/contact-us', [LandingPageController::class, 'contact'])->name('landingpage.contact');
